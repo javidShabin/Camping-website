4853a71ee66a0e9f7a9620c67c464e4f42aac210
javid shabin <javid.main.act@gmail.com>
2024-05-02 17:55:23 +0530

    link with js and make menu function with js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+let menuBtn = document.querySelector(".menu-btn");
+let navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("open")
+})
